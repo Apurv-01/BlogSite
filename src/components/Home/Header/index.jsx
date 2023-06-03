cf1ad36fb07722cfeea85c229ae762627ff518a5
Apurv <31apurv@gmail.com>
2023-06-03 15:28:25 +0530

    Commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      Express Yourself
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,75 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'What is Full Stack Web Development?',
+    category: 'development',
+    subCategory: ['frontend', 'ui/ux', 'design'],
+    description:
+      "Full stack development refers to the end-to-end application software development, including the front end and back end. The front end consists of the user interface, and the back end takes care of the business logic and application workflows.",
+    authorName: 'Eren Yeager',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '01 July, 2022',
+    cover: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png',
+  },
+  {
+    id: 2,
+    title: 'Places to visit India',
+    category: 'travel',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+      "India, officially the Republic of India, is a country in South Asia. It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world",
+    authorName: 'Light Yagami',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '01 July, 2022',
+    cover: 'https://www.savoredjourneys.com/wp-content/uploads/2017/04/india-places-to-visit.jpg',
+  },
+  {
+    id: 3,
+    title: 'Online Shopping Boon or Bane',
+    category: 'shopping',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Naruto Uzumaki',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '15 July, 2022',
+    cover: 'https://miro.medium.com/v2/resize:fit:1018/1*iAu65xDmvpVdBJgps6EDEw.png',
+  },
+  {
+    id: 4,
+    title: 'Gaming is going real in 2025',
+    category: 'gaming',
+    subCategory: ['gaming', 'pc', 'lifestyle'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Kojima',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '01 August, 2022',
+    cover: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/12/elden-ring-cover.jpg',
+  },
+  {
+    id: 5,
+    title: 'Beautiful Beaches of Goa',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Johnny Depp',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '01 July, 2022',
+    cover:
+      'https://static.toiimg.com/thumb/msid-55310626,width-748,height-499,resizemode=4,imgsize-175052/The-best-beaches-of-Goa.jpg',
+  },
+  {
+    id: 6,
+    title: 'The World of Virtual Reality',
+    category: 'tech',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Picasso',
+    authorAvatar: 'https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png',
+    createdAt: '02 July, 2022',
+    cover: 'https://stereoscape.com/wp-content/uploads/2023/02/10-reasons-for-vr-training-scaled-e1684316724261.jpeg',
+  },
+];
